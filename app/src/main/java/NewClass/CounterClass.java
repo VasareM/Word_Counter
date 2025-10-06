ee06ba33cef4eacaa42be3c1ea7db7abca28d3c7
VasareM <vasare.milkovaite@mif.stud.vu.lt>
2025-10-06 22:46:36 +0300

    preliminarus vaizdas

---
+++ app/src/main/java/NewClass/CounterClass.java
@@ -0,0 +1,8 @@
+package NewClass;
+
+public class CounterClass {
+    public static int getCharsCount(String userInput) {
+        return userInput.length();
+    }
+}
+// static: nereikia kurti klasės instance'ų; kai nererikia accessint objekto; mažiau kodo
--- app/src/main/java/com/example/wordcounter/MainActivity.java
+++ app/src/main/java/com/example/wordcounter/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.wordcounter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import NewClass.CounterClass;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvCountResult;
+
+    EditText edUserInput;
+
+    Spinner spOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        this.tvCountResult = findViewById(R.id.tvCountResult);
+        this.edUserInput =  findViewById(R.id.edUserInput);
+
+        this.spOptions = findViewById(R.id.spOptions);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.count_options,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spOptions.setAdapter(adapter);
+    }
+
+    public void onBtnCountClick(View view) {
+        if(this.spOptions.getSelectedItem().toString().equalsIgnoreCase(getString(R.string.chars_selection))) {
+            String userPhrase = this.edUserInput.getText().toString();
+            int charsCount = CounterClass.getCharsCount(userPhrase);
+            this.tvCountResult.setText(String.valueOf(charsCount));
+        }
+
+        else {
+            this.tvCountResult.setText("Not implemented");
+        }
     }
 }
